bd2535e446cef0628a77ccc3510ebe0f73868546
Jihen BOUKHADHRA <129115407+Jihen-Boukhadhra@users.noreply.github.com>
2024-05-24 23:21:55 +0100

    Create main.c

---
+++ main.c
@@ -0,0 +1,87 @@
+
+int main()
+{
+VISITE visites;
+YED nedisaments;
+int n, k;
+printf("lir la nostre de patients")
+scanf
+F PATIENT allont sizeof(PATIENT))
+if (pl
+fer (int i
+printf("
+printf("in aalaix les infos da patient 4", 1+1)
+printf("six la nn et la penom")
+acant,
+printf("
+acanf(""
+p + 1)-
+aiz le zeze da patient");
+(p+1)
+printf(" si la profession"
+acant "", p +1)->process)
+printf("\n zais la date de naissance");
+scant("//" (p+)->dat_asjon, tp-date_aisais,
+ip-datais.
+printf zalai ");
+scanf("%d", sp+)->)
+printf("\n is sondes de til ");
+scanf("%d", (p+i)-> telephone)
+printf(" zalait le akts de ite: "
+scanf("%", sp 1)->_viaites)
+visites (VISITE alloc((p +i)->_visites sizeof(VIE)
+x1:1-2)
+for (int -1<(p+1)- visites 5++
+printf(" saisir les visites
+printf saisir la date de
+"j+)
+d-djel
+scanf("//", visites 1) data V.jour,
+(vialtos 31->date Roxoia, visites 11-date _RDV.)
+printf(" lei lei de a
+scanf("d", visites -j-bredients);
+printf(" saisir les dicatat
+medicaments (MED)malloc((visites 1)->nbredicaments sizeof (MED))
+if (Inedicamenta)
+exit(-3)
+for (int ko (visites +1)->nr medicaments; k++) {
+printf("\n saisir la référence du sédicament +11)
+acant ("d",
+medicaments)->reference)
+printf("\n is no da a\);
+-(+)
+sintf("\n affichage du tableaux des patients");
+for (int - 1++
+printf("\n--
+printf("in loa infos du patient d", 1+11
+printf("a le as splet at a ", (pi)->_pen)
+printf("a le axe du patient oat a, (p+1-
+printf("\n la profession est
+(p+1)->profession)
+printf("\n la date de naissasos est d/d/d, (p1)-date nales.jour,
+(p+11-date nalas.mola. (p+1)-date nalas.anne)
+printf("\n son age wat d", (p+1)->age)
+print("\n son uno de los est d" (p+1)-) telephone)
+printf("\n le sobre de xisite eat d", (pi)->mb_vinite);
+
+
+
+printf("\n le nombre de xiaites est d", (p +1)->nbr_visitas);
+printf("\n les visites du patient \n");
+for (int 1 0 1 < (p+1)->nbr_visites; j++) (
+printf("\n la date du rendez-vous est d/d/d",
+(visites +j)->date RDV. jour, (visitas +1)->date_RDV.mois,
+(visites )->date REV. annse)/
+printf("\n le nombra de médicamanta ast d",
+(visites )->nlr_medicaments),
+printf("\n voici asa médicamenta\n");
+for (int k 0 k < (visitos +1)->nbr_medicaments; k++) (
+printf("\n la référence du médicament d est 8d", k+1,
+(medicaments + k)->reference)
+printf("\n le nom du médicament eat so",
+(medicaments + k)->nom medicament);
+}
+}
+}
+return Or;
+  }
